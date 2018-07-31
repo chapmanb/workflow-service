312ff4a9785c42b67b9231fcd3f9094b8a0029b8
chapmanb <chapmanb@50mail.com>
2018-07-31 14:43:02 -0400

    wes-client without wes-server dependencies
    Provides ability to install and use wes-client without needing
    server-only utilities: connexion, cwlref-runner, subprocess and
    friends.
    
    Moves wes_client to standard script instead of entry_point to
    avoid requirements issues. On conda installs wes-client via
    pkg_resources gets confused with msgpack and optional inputs
    for jsonschema.
    
    Adds python3 compatibility to wes-client script.

--- setup.py
+++ setup.py
@@ -19,23 +19,24 @@ setup(name='wes-service',
       url="https://github.com/common-workflow-language/cwltool-service",
       download_url="https://github.com/common-workflow-language/cwltool-service",
       license='Apache 2.0',
-      packages=["wes_service", "wes_client"],
+      packages=["wes_service"],
       package_data={'wes_service': ['openapi/workflow_execution_service.swagger.yaml']},
       include_package_data=True,
       install_requires=[
           'future',
-          'connexion==1.4.2',
           'bravado==10.1.0',
           'ruamel.yaml >= 0.12.4, < 0.15',
-          'cwlref-runner==1.0',
           'schema-salad>=2.6, <3',
-          'subprocess32==3.5.2'
                         ],
       entry_points={
-          'console_scripts': ["wes-server=wes_service.wes_service_main:main",
-                              "wes-client=wes_client.wes_client_main:main"]
+          'console_scripts': ["wes-server=wes_service.wes_service_main:main[wes_service]"]
                     },
+      scripts=["scripts/wes-client"],
       extras_require={
+          "wes_service": ['connexion==1.4.2',
+                          'cwlref-runner==1.0',
+                          'subprocess32==3.5.2'
+                         ],
           "arvados": ["arvados-cwl-runner"
                       ],
           "toil": ["toil[all]==3.16.0"
